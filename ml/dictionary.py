41e52a71b0e5882694a288338f2f74aaeeffc051
GabrielmAlves <gabrielmalves10@gmail.com>
2026-04-21 18:22:30 -0300

    [Dictionary] Update dictionary

--- ml/dictionary.py
+++ ml/dictionary.py
@@ -1,5 +1,63 @@
 vague_terms = {
-    "duracao": ["curto", "longo", "meio curto", "médio", "meio longo"],
-    "intensidade": ["muito", "bem", "pouco"],
-    "qualidade": ["engraçado", "emocionante", "pesado", "triste", "alegre", "feliz", "tenso"]
+    "duracao": ["curto",
+                "curtinho",
+                "meio curto",
+                "longo",
+                "longo demais",
+                "meio longo", 
+                "médio"
+                ],
+    
+    "intensidade": ["muito", 
+                    "bem", 
+                    "pouco",
+                    "super",
+                    "extremamente",
+                    "um pouco",
+                    "meio",
+                    "levemente"
+                    ],
+    
+    "emocao": ["engraçado",
+               "hilário",
+               "divertido",
+               "emocionante",
+               "tocante",
+               "triste",
+               "depressivo",
+               "alegre",
+               "feliz",
+               "tenso",
+               "angustiante",
+               "assustador",
+               "leve"],
+    
+    "qualidade": ["bom",
+                  "muito bom",
+                  "ótimo",
+                  "excelente",
+                  "ruim",
+                  "fraco",
+                  "mais ou menos",
+                  "top",
+                  "legal"
+                  ],
+    
+    "ritmo": ["lento",
+              "parado",
+              "rápido",
+              "dinâmico",
+              "arrastado"
+              ],
+    
+    "complexidade": ["simples",
+                     "fácil",
+                     "complexo",
+                     "difícil",
+                     "profundo"
+                     ],
+    "popularidade": ["famoso",
+                     "popular",
+                     "desconhecido"
+                     ]
 }
