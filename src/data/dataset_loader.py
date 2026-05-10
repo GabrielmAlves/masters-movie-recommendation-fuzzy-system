b16da028d188056f4c883042d1acf120f9e649ab
GabrielmAlves <gabrielmalves10@gmail.com>
2026-05-10 20:31:14 -0300

    [dataset] update way dataset is read

--- src/data/dataset_loader.py
+++ src/data/dataset_loader.py
@@ -1,44 +1,56 @@
 import pandas as pd
-import ast
 from data.movie import Movie
 
 def load_movies_database(path: str) -> list[Movie]:
-    dataframe = pd.read_csv(path, low_memory=False)
-    
-    dataframe = dataframe.sample(frac=0.01, random_state=42)
-    
+
+    columns = [
+        "title",
+        "runtime",
+        "overview",
+        "genres",
+        "release_date",
+        "adult",
+        "popularity",
+        "vote_count",
+        "vote_average"
+    ]
+
     movies = []
-    
-    for _, row in dataframe.iterrows():
-        try:
-            title = row["title"]
-            duration = row["runtime"]
-            overview = row["overview"]
-            genres = row["genres"]
-            release_date = row["release_date"]
-            is_adult = row["adult"]
-            popularity = row["popularity"]
-            vote_count = row["vote_count"]
-            vote_average = row["vote_average"]
-            
-            if pd.isna(title) or pd.isna(duration) or pd.isna(overview):
+
+    chunks = pd.read_csv(
+        path,
+        usecols=columns,
+        chunksize=10000
+    )
+
+    for chunk in chunks:
+
+        chunk = chunk.sample(frac=0.01)
+
+        for _, row in chunk.iterrows():
+            try:
+                if (
+                    pd.isna(row["title"]) or
+                    pd.isna(row["runtime"]) or
+                    pd.isna(row["overview"])
+                ):
+                    continue
+
+                movie = Movie(
+                    title=row["title"],
+                    release_date=row["release_date"],
+                    duration=float(row["runtime"]),
+                    adult=row["adult"],
+                    overview=row["overview"],
+                    popularity=row["popularity"],
+                    genres=row["genres"],
+                    vote_count=row["vote_count"],
+                    vote_average=row["vote_average"]
+                )
+
+                movies.append(movie)
+
+            except Exception:
                 continue
-            
-            movie = Movie(
-                title=title,
-                release_date=release_date,
-                duration=float(duration),
-                adult=is_adult,
-                overview=overview,
-                popularity=popularity,
-                genres=genres,
-                vote_count=vote_count,
-                vote_average=vote_average
-            )
-            
-            movies.append(movie)
-            
-        except Exception:
-            continue
-    
+
     return movies
