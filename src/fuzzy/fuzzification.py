38d2a208b6df0239cdc89a4b7186560e6333a536
GabrielmAlves <gabrielmalves10@gmail.com>
2026-05-24 19:25:17 -0300

    add distribution images, create funny and action sets, apply some hedges

--- src/fuzzy/fuzzification.py
+++ src/fuzzy/fuzzification.py
@@ -1,5 +1,5 @@
 from fuzzy.linguistic_variables import duration_sets
-from fuzzy.emotion_variables import tension_sets
+from fuzzy.emotion_variables import tension_sets, funny_sets, action_sets
 
 def fuzzify_duration(duration):
     membership_values = {}
@@ -13,4 +13,16 @@ def fuzzify_tension(tension_score):
     return {
         term: fn(tension_score)
         for term, fn in tension_sets.items()
+    }
+
+def fuzzify_funny(funny_score):
+    return {
+        term: fn(funny_score)
+        for term, fn in funny_sets.items()
+    }
+
+def fuzzify_action(action_score):
+    return {
+        term: fn(action_score)
+        for term, fn in action_sets.items()
     }
--- src/ranking/ranker.py
+++ src/ranking/ranker.py
@@ -1,48 +1,79 @@
 from fuzzy.hedges import apply_hedge
-from fuzzy.fuzzification import fuzzify_duration, fuzzify_tension
+from fuzzy.fuzzification import (
+    fuzzify_duration,
+    fuzzify_tension,
+    fuzzify_funny
+)
 from fuzzy.operators import fuzzy_and
 
 def compute_movie_score(movie, interpreted_query):
     scores = []
-    
+
     if "duração" in interpreted_query:
         duration_term = interpreted_query["duração"]
-        
-        duration_score = fuzzify_duration(movie.duration)[duration_term]
+
+        duration_score = fuzzify_duration(
+            movie.duration
+        )[duration_term]
+
         scores.append(duration_score)
-    
+
     if "emoção" in interpreted_query:
         emotion = interpreted_query["emoção"]
-        
+
+        # ------------------------
+        # TENSÃO
+        # ------------------------
+
         if emotion in ["assustador", "tenso"]:
+
             emotion_score = fuzzify_tension(
                 movie.tense_score
             )["tenso"]
-        elif emotion == "engraçado":
-            emotion_score = movie.funny_score
+
+        # ------------------------
+        # HUMOR
+        # ------------------------
+
+        elif emotion in ["engraçado", "hilário", "divertido"]:
+
+            emotion_score = fuzzify_funny(
+                movie.funny_score
+            )["engraçado"]
+
         else:
             emotion_score = 0
-        
+
+        # ------------------------
+        # HEDGE
+        # ------------------------
+
         if "intensidade" in interpreted_query:
+
             emotion_score = apply_hedge(
                 emotion_score,
                 interpreted_query["intensidade"]
             )
+
         scores.append(emotion_score)
-    
+
     if not scores:
         return 0
-    
+
     final_score = scores[0]
-    
+
     for score in scores[1:]:
-        final_score = fuzzy_and(final_score, score)
-        
+        final_score = fuzzy_and(
+            final_score,
+            score
+        )
+
     return final_score
 
+
 def rank_movies(movies, interpreted_query):
-    
-     return sorted(
+
+    return sorted(
         movies,
         key=lambda m: compute_movie_score(
             m,
